88da0ede554ef78a9718a7376250aade3c55a48b
Harsh Srivastava <srivastavaharsh8957@gmail.com>
2023-08-20 22:46:45 +0530

    created pizza billing

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,12 +3,17 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
+    final private Boolean isVeg;
     private String bill;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg) {
+            this.price += 300;
+        }else {
+            this.price += 400;
+        }
     }
 
     public int getPrice(){
@@ -17,18 +22,26 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        this.price += 80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg) {
+            this.price += 70;
+        }else{
+            this.price += 120;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.price += 20;
     }
 
     public String getBill(){
         // your code goes here
+        this.bill = "Total Price: "+this.price;
         return this.bill;
     }
 }
